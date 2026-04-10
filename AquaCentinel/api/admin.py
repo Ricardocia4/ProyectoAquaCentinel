e47460b985c38d5f34cf412ec4765d42f40210fc
Ricardocia4 <garciaazcorra.ricardo@gmail.com>
2026-04-09 20:49:21 -0600

    admin listo

--- AquaCentinel/api/admin.py
+++ AquaCentinel/api/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Boya
 
-admin.site.register(Boya)
+
+@admin.register(Boya)
+class BoyaAdmin(admin.ModelAdmin):
+    # 1. Definimos qué columnas ver en la tabla principal
+    list_display = ("codigo_boya", "usuario")
+
+    # 2. Excluimos 'usuario' del formulario para que no aparezca
+    exclude = ("usuario",)
+
+    # 3. Hacemos que el código sea visible pero no editable en el detalle
+    readonly_fields = ("codigo_boya",)
--- AquaCentinel/api/models.py
+++ AquaCentinel/api/models.py
@@ -1,18 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User  # Importamos el modelo estándar
 from django.core.validators import MinValueValidator
+import uuid
 
 
 class Boya(models.Model):
-    codigo_boya = models.CharField(max_length=100, unique=True)
-    # Relación directa al modelo User de Django
+    # 'editable=False' lo oculta de los formularios por defecto
+    class Meta:
+        db_table = "sensores_boya"
+
+    codigo_boya = models.CharField(max_length=100, unique=True, editable=True)
     usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        # Solo generamos el código si la boya es nueva (no tiene ID aún)
+        if not self.pk and not self.codigo_boya:
+            # Genera un código único basado en UUID (ej: AQUA-A1B2C3D4)
+            nuevo_id = str(uuid.uuid4()).split("-")[0].upper()
+            self.codigo_boya = f"AQUA-{nuevo_id}"
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.codigo_boya
 
 
 class RegistroSensor(models.Model):
+
     boya = models.ForeignKey(Boya, on_delete=models.CASCADE)
     ph = models.FloatField(validators=[MinValueValidator(0.0)])
     turbidez = models.FloatField(validators=[MinValueValidator(0.0)])
